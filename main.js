68f8c26221bedf884f035bbec9027e92ed65bbe7
Aymen Dirar <adirar111@gmail.com>
2020-07-07 20:48:52 -0700

    some tweaks

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
+// let smallDisplay = window.matchMedia("(max-width: 1024px)");
+// let icons = document.querySelectorAll(".fab");
 
+// function resizeIcons(small) {
+//     console.log(icons);
+//     if (small.matches) {
+//         console.log("make smoll");
+//         console.log(icons[0]);
+//         icons.forEach((i) => {
+//             if (i.classList.contains("fa-6x")) {
+//                 i.classList.replace("fa-6x", "fa-3x");
+//                 console.log("big->small");
+//             } else {
+//                 i.classList.add("fa-3x");
+//             }
+//         });
+//         console.log("smollify done");
+//     } else {
+//         console.log(icons[0]);
+//         console.log("make beeg");
+//         icons.forEach((i) => {
+//             if (i.classList.contains("fa-3x")) {
+//                 i.classList.replace("fa-3x", "fa-6x");
+//                 console.log("small->big");
+//             } else {
+//                 i.classList.add("fa-6x");
+//             }
+//         });
+//         console.log("emgenning done");
+//     }
+// }
+
+// resizeIcons(smallDisplay);
+// smallDisplay.addListener(resizeIcons);
