bc5fb7690e25be092807c2e5d0fc31d56afeb45e
Aymen Dirar <adirar111@gmail.com>
2020-07-07 22:57:25 -0700

    second column not overflowing

--- main.js
+++ main.js
@@ -1,34 +1,21 @@
-// let smallDisplay = window.matchMedia("(max-width: 1024px)");
-// let icons = document.querySelectorAll(".fab");
+window.addEventListener("DOMContentLoaded", (event) => {
+    // credit to https://codepen.io/lasjorg/pen/GRKreyJ
+    const button = document.querySelector("#play-pause");
+    const icon = document.querySelector("#volume-icon");
+    const audio = document.querySelector("audio");
 
-// function resizeIcons(small) {
-//     console.log(icons);
-//     if (small.matches) {
-//         console.log("make smoll");
-//         console.log(icons[0]);
-//         icons.forEach((i) => {
-//             if (i.classList.contains("fa-6x")) {
-//                 i.classList.replace("fa-6x", "fa-3x");
-//                 console.log("big->small");
-//             } else {
-//                 i.classList.add("fa-3x");
-//             }
-//         });
-//         console.log("smollify done");
-//     } else {
-//         console.log(icons[0]);
-//         console.log("make beeg");
-//         icons.forEach((i) => {
-//             if (i.classList.contains("fa-3x")) {
-//                 i.classList.replace("fa-3x", "fa-6x");
-//                 console.log("small->big");
-//             } else {
-//                 i.classList.add("fa-6x");
-//             }
-//         });
-//         console.log("emgenning done");
-//     }
-// }
-
-// resizeIcons(smallDisplay);
-// smallDisplay.addListener(resizeIcons);
+    button.addEventListener("click", () => {
+        console.log("click");
+        if (audio.paused) {
+            audio.volume = 0.4;
+            audio.play();
+            icon.classList.remove("fa-volume-up");
+            icon.classList.add("fa-volume-mute");
+        } else {
+            audio.pause();
+            icon.classList.remove("fa-volume-mute");
+            icon.classList.add("fa-volume-up");
+        }
+        button.classList.add("fade");
+    });
+});
