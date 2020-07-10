e49a1633f6da3cdcd5cc5abebe150beadda7d382
Aymen Dirar <adirar111@gmail.com>
2020-07-09 21:23:14 -0700

    positioning improvements, text and colors needed

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-    // credit to https://codepen.io/lasjorg/pen/GRKreyJ
+    // credit: https://codepen.io/lasjorg/pen/GRKreyJ
     const button = document.querySelector("#play-pause");
-    const icon = document.querySelector("#volume-icon");
     const audio = document.querySelector("audio");
 
     button.addEventListener("click", () => {
@@ -9,12 +8,8 @@ window.addEventListener("DOMContentLoaded", (event) => {
         if (audio.paused) {
             audio.volume = 0.4;
             audio.play();
-            icon.classList.remove("fa-volume-up");
-            icon.classList.add("fa-volume-mute");
         } else {
             audio.pause();
-            icon.classList.remove("fa-volume-mute");
-            icon.classList.add("fa-volume-up");
         }
         button.classList.add("fade");
     });
